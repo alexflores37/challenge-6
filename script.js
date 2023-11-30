01578c5baaecd0ac79b550f4a65675444c130231
Arianna Contreras Flores <ariannacontrerasflores@Ariannas-MacBook-Pro.local>
2023-11-30 11:12:40 -0700

    new changes

--- script.js
+++ script.js
@@ -1,37 +1,60 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    async function fetchWeather(city, units) {
+        const apiKey = 'b438e216c6538eabde5fc556a9a19826';
+        const apiUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}';
 
-const apiKey = 'b438e216c6538eabde5fc556a9a19826';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-const city = 'Tucson,AZ';
-const units = 'metric'; 
-async function fetchWeather() {
-    try {
-        const response = await fetch(`${apiUrl}?q=${city}&units=${units}&appid=${apiKey}`);
-        const data = await response.json();
-        displayForecast(data.list);
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
+        try {
+            const response = await fetch(`${apiUrl}?q=${city}&units=${units}&appid=${apiKey}`);
+            const data = await response.json();
+            displayForecast(data.list);
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+        }
     }
-}
 
-function displayForecast(forecastData) {
-    const forecastContainer = document.getElementById('forecast-container');
 
-    forecastData.forEach(item => {
-        const date = new Date(item.dt * 1000);
-        const day = date.toLocaleDateString('en-US', { weekday: 'long' });
-        const temp = item.main.temp;
-        const description = item.weather[0].description;
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const cityInput = document.getElementById('city-input');
+        const unitsSelect = document.getElementById('units-select');
 
-        const forecastCard = document.createElement('div');
-        forecastCard.classList.add('forecast-card');
-        forecastCard.innerHTML = `
-            <p>${day}</p>
-            <p>${temp}°C</p>
-            <p>${description}</p>
-        `;
+        const city = cityInput.value;
+        const units = unitsSelect.value;
 
-        forecastContainer.appendChild(forecastCard);
-    });
-}
+       
+        const forecastContainer = document.getElementById('forecast-container');
+        forecastContainer.innerHTML = '';
 
-fetchWeather();
+      
+        fetchWeather(city, units);
+    }
+
+    function displayForecast(forecastData) {
+        const forecastContainer = document.getElementById('forecast-container');
+
+        forecastData.forEach(item => {
+            const date = new Date(item.dt * 1000);
+            const day = date.toLocaleDateString('en-US', { weekday: 'long' });
+            const temp = item.main.temp;
+            const description = item.weather[0].description;
+
+            const forecastCard = document.createElement('div');
+            forecastCard.classList.add('forecast-card');
+            forecastCard.innerHTML = `
+                <p>${day}</p>
+                <p>${temp}°C</p>
+                <p>${description}</p>
+            `;
+
+            forecastContainer.appendChild(forecastCard);
+        });
+    }
+
+   
+    const form = document.getElementById('forecast-form');
+    form.addEventListener('submit', handleFormSubmit);
+
+   
+    fetchWeather('Tucson_Az', 'imperial'); 
+})
